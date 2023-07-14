0b5a2b7851cdbcef8bcbac344d4e2cc44713f441
nik0208 <nikita199080@icloud.com>
2023-07-14 15:43:08 +0600

    140720231543

--- directories/urls.py
+++ directories/urls.py
@@ -4,7 +4,8 @@ from .views import *
 urlpatterns = [
     path('os/', Os, name='os'),
     path('tmc/', Tmc, name='tmc'),
-    path('upload-data/', upload_data, name='upload_data'),
+    path('upload-data-os/', upload_data_os, name='upload_data_os'),
+    path('upload-data-tmc/', upload_data_tmc, name='upload_data_tmc'),
     path('os_list/', OsList.as_view(), name='os_list'),
     path('tmc_list/', TmcList.as_view(), name='tmc_list')
 ]
