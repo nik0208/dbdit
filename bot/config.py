5c9f68ce1be5c16727fe5c19a252ce78bb1c2be2
cpra <cpra@technodom.kz>
2023-08-03 14:27:48 +0600

    142703082023

---
+++ bot/config.py
@@ -0,0 +1,2 @@
+
+token = '6083458782:AAHce7jw-k4EsQFmolRncB-b3u1B_EH-syg'
--- yandex_go/views.py
+++ yandex_go/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . import models
+from django.apps import apps
+from openpyxl import load_workbook
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django_datatables_view.base_datatable_view import BaseDatatableView
+import os
+from django.conf import settings
+import subprocess
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from docxtpl import DocxTemplate
+import pandas as pd
+import requests
+import json
 
-# Create your views here.
+@login_required
+
+def TaxiRequest(requests):
+
+    url = 'https://b2b-api.go.yandex.ru/integration/2.0/orders/create'
+
+    headers = {'Authorization': 'Bearer <OAuth-токен>',}
+
+    data = {
+    "route": [
+        [start_point],
+        [destination_point]
+    ],
+    "user_id": "035...3c71"
+  }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    print(response.status_code)
+    print(response.text)
