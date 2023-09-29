1baec6e2c9d369462c63ed8ac0fce8bd0192a0cc
nik0208 <nikita199080@icloud.com>
2023-09-29 08:36:20 +0600

    290920230836

--- dbdit/wsgi.py
+++ dbdit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ditdb project.
+WSGI config for dbdit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ditdb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbdit.settings')
 
 application = get_wsgi_application()
