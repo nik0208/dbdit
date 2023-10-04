f3ab666317020570a9f575fc5c39b278401ce27b
nik0208 <nikita199080@icloud.com>
2023-10-04 12:57:01 +0600

    04101256

--- directories/urls.py
+++ directories/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('os/', Os, name='os'),
     path('tmc/', Tmc, name='tmc'),
-    path('upload-data-os/', upload_data_os, name='upload_data_os'),
-    path('upload-data-tmc/', upload_data_tmc, name='upload_data_tmc'),
     path('os_list/', OsList.as_view(), name='os_list'),
     path('tmc_list/', TmcList.as_view(), name='tmc_list'),
     
--- loadsql/views.py
+++ loadsql/views.py
@@ -8,12 +8,42 @@ import csv
 from directories.models import *
 from acts.models import *
 from django.db import transaction
+import subprocess
+from django.db import connection
 import openpyxl
-
 
 @login_required
 def Loadsqls(request):
     return render(request, 'loadsql/loadsql.html')
+
+
+def get_table_columns(table_name):
+    with connection.cursor() as cursor:
+        cursor.execute(f'PRAGMA table_info({table_name});')
+        columns = cursor.fetchall()
+        column_names = [column[1] for column in columns]
+    return column_names
+
+
+def reorder_csv_columns(input_file, output_file, column_order):
+    with open(input_file, 'r', encoding='utf-8') as infile:
+        reader = csv.DictReader(infile)
+        
+        with open(output_file, 'w', encoding='utf-8', newline='') as outfile:
+            writer = csv.DictWriter(outfile, fieldnames=column_order)
+            writer.writeheader()
+            
+            for row in reader:
+                reordered_row = {key: row[key] for key in column_order}
+                writer.writerow(reordered_row)
+
+
+def import_csv_to_sqlite(csv_file_path, db_name, table_name):
+        # Команда для выполнения импорта CSV в SQLite
+        command = f'sqlite3 "{db_name}" ".mode csv" ".import {csv_file_path} {table_name}"'
+
+        # Запуск команды в терминале
+        subprocess.run(command, shell=True)
 
 
 def upload_file(request):
@@ -32,6 +62,79 @@ def upload_file(request):
         file_extension = os.path.splitext(file.name)[1].lower()
 
         if file_extension != '.csv':
+            if selected_option == 'IT_OS':
+                wb = openpyxl.load_workbook(file_path)
+                sheet = wb.active  # Выбираем активный лист
+
+                column_number = None
+                for col in sheet.iter_cols(min_row=1, max_row=1):
+                    if col[0].value == "Основное средство.Инв. номер ДИТ":
+                        column_number = col[0].col_idx  # Получаем номер столбца
+                        break
+
+                if column_number:
+                    # Вставляем новый столбец сразу после найденного
+                    sheet.insert_cols(column_number + 1)
+
+                    # Дать имя новому столбцу
+                    sheet.cell(row=1, column=column_number + 1).value = "Группа ОС"
+
+                    # Проходимся по строкам
+                    for row_num in range(2, sheet.max_row + 1):
+                        cell_value = sheet.cell(row=row_num, column=column_number).value
+                        if cell_value is not None:
+                            first_five_chars = cell_value[:5]
+                            
+                            new_value = None
+                            if first_five_chars == "ITEKS":
+                                new_value = "Мини ПК ITEKS"
+                            elif first_five_chars == "ITMNT":
+                                new_value = "Мониторы ITMNT"
+                            elif first_five_chars == "ITMNB":
+                                new_value = "Моноблок ITMNB"
+                            elif first_five_chars == "ITNTB":
+                                new_value = "Ноутбук ITNTB"
+                            elif first_five_chars == "ITPAD":
+                                new_value = "Планшет ITPAD"
+                            elif first_five_chars == "ITSRV":
+                                new_value = "Сервер ITSRV"
+                            elif first_five_chars == "ITSHD":
+                                new_value = "Система хранения данных ITSHD"
+                            elif first_five_chars == "ITWKS":
+                                new_value = "Системный блок, Тонкий клиент ITWKS"
+                            elif first_five_chars == "ITVDN":
+                                new_value = "Видеорегистраторы ITVDN"
+                            elif first_five_chars == "ITHDD":
+                                new_value = "Жесткие диски ITHDD"
+                            elif first_five_chars == "ITUPS":
+                                new_value = "ИБП (источники бесперебойного питания) Стабилизатор ITUPS"
+                            elif first_five_chars == "ITKSS":
+                                new_value = "Кассовое оборудование ITKSS"
+                            elif first_five_chars == "ITSAN":
+                                new_value = "Оптические коммутаторы ITSAN"
+                            elif first_five_chars == "ITVDC":
+                                new_value = "Охранное видеонаблюдение (Видеокамеры ITVDC)"
+                            elif first_five_chars == "ITPRN":
+                                new_value = "Принтеры, МФУ, копировальные аппараты ITPRN"
+                            elif first_five_chars == "ITPRK":
+                                new_value = "Проектор ITPRK"
+                            elif first_five_chars == "ITETH" or first_five_chars == "ITCSC":
+                                new_value = "Сетевое оборудование ITETH"
+                            elif first_five_chars == "ITSCN":
+                                new_value = "Сканеры штрихкода ITSCN"
+                            elif first_five_chars == "ITCNT" or first_five_chars == "ITKND":
+                                new_value = "Счетчики посетителей ITCNT"
+                            elif first_five_chars == "ITTLF":
+                                new_value = "Телефон, факс ITTLF"
+                            elif first_five_chars == "ITTCD":
+                                new_value = "Терминал для сбора данных ITTCD,  подставки под ТСД, зарядные устройства для ТСД"
+                            elif first_five_chars == "ITBOX":
+                                new_value = "Шкаф коммутационный, серверный  ITBOX"
+
+                        if new_value:
+                                sheet.cell(row=row_num, column=column_number + 1).value = new_value
+
+                wb.save(file_path)
             try:
                 # Чтение данных из Excel-файла
                 data = pd.read_excel(file_path, engine='openpyxl')
@@ -62,6 +165,7 @@ def upload_file(request):
                             position=row['Должность'],
                             organization=row['Организация'],
                             subdivision=row['Подразделение']
+                            
                         )
             os.remove(file_path)
             return redirect('/')
@@ -98,5 +202,35 @@ def upload_file(request):
                         )
             os.remove(file_path)
             return redirect('/directories/tmc')
+        
+        elif selected_option == 'IT_OS':
+
+            column_name = get_table_columns(selected_option)
+            mapping = {
+                'inv_dit': 'Основное средство.Инв. номер ДИТ',
+                'name_os': 'Основное средство.Наименование',
+                'inpute_date': 'Дата принятия к учету',
+                'original_price': 'Первоначальная стоимость',
+                'serial_number': 'Основное средство.Номер паспорта (регистрационный)',
+                'os_group': 'Группа ОС',
+            }
+            column_order = []
+            for i in range(len(column_name)):
+                column_order.append(mapping.get(column_name[i], None))
+            
+            output_file_path = 'file.csv'
+            reorder_csv_columns(file_path, output_file_path, column_order)
+            
+            db_name = 'db.sqlite3'
+            import_csv_to_sqlite(output_file_path, db_name, selected_option)
+            
+            os.remove(file_path)
+            os.remove(output_file_path)
+
+            return redirect('/directories/os')
+        
+
+
+
 
         return redirect('/loadsql')
