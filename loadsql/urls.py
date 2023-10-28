f5f77a7432d24dc11cabe78f516d9d59552efd2e
nik0208 <nikita199080@icloud.com>
2023-10-28 12:16:51 +0600

    3214215r

--- loadsql/urls.py
+++ loadsql/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('', Loadsqls, name='loadsql'),
     path('upload/', upload_file, name='upload_file'),
-    path('add_act_skans/', add_act_skans, name='add_act_skans'),
+    # path('add_act_skans/', add_act_skans, name='add_act_skans'),
 ]
